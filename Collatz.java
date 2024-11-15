1cb99df739c4df6842d0d8f720345984e2b21d7d
Etay Cohen <etay@altahq.com>
2024-11-15 17:16:21 +0200

    Completed Collatz

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,28 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int iterations = Integer.parseInt(args[0]);
+		boolean isVerbose = args[1].equals("v");
+
+		for (int i = 1; i <= iterations; i++) {
+			int seed = i;
+			int count = 1;
+
+			String verboseOutput = seed + " ";
+
+			do {
+				seed = seed % 2 == 0 ? seed / 2 : 3 * seed + 1;
+				if (isVerbose) {
+					verboseOutput += seed + " ";
+				}
+				count++;
+			} while (seed != 1);
+
+			if (isVerbose) {
+				System.out.println(String.format("%s(%d)", verboseOutput, count));
+			}
+		}
+
+		System.out.println(String.format("Every one of the first %d hailstone sequences reached 1.", iterations));
 	}
 }
