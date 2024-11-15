cc2943225551844232945e5e67e75846f49f7cfc
Etay Cohen <etay@altahq.com>
2024-11-15 17:30:17 +0200

    added missing arg to Cheers

--- Cheers.java
+++ Cheers.java
@@ -5,6 +5,8 @@ public class Cheers {
 
 		String cheer = args[0].toUpperCase();
 
+		int cheersCount = Integer.parseInt(args[1]);
+
 		for (int i = 0; i < cheer.length(); i++) {
 			boolean isSpecialChar = letters.indexOf(cheer.charAt(i)) != -1;
 			String prefix = isSpecialChar ? "Give me an" : "Give me a ";
@@ -14,7 +16,7 @@ public class Cheers {
 
 		System.out.println("What does that spell?");
 
-		for (int i = 0; i < cheer.length(); i++) {
+		for (int i = 0; i < cheersCount; i++) {
 			System.out.println(cheer + "!!!");
 		}
 	}
