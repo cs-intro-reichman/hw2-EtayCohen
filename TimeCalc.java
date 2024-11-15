08751ffdef41a1032c524e4123ad40e9f5f4f1b2
Etay Cohen <etay@altahq.com>
2024-11-15 15:23:43 +0200

    Completed TimeCalc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,14 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt(args[0].substring(0, 2));
+        int minutes = Integer.parseInt(args[0].substring(3, 5));
+
+        int minutesToAdd = Integer.parseInt(args[1]);
+
+        int cuurentTimeInMinutes = hours * 60 + minutes + minutesToAdd;
+        int newMinutes = cuurentTimeInMinutes % 60;
+        int newHours = (cuurentTimeInMinutes / 60) % 24;
+
+        System.out.println(String.format("%d:%02d", newHours, newMinutes));
     }
 }
